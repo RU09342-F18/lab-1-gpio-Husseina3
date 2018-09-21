33d81d11dbeb5eb1f456db7385dd34e301d35d29
Husseina3 <35783056+Husseina3@users.noreply.github.com>
2018-09-21 19:19:02 -0400

    Update main.c

--- Button Blink/MSP430F5529/main.c
+++ Button Blink/MSP430F5529/main.c
@@ -12,16 +12,16 @@
 
 
   P1DIR |= BIT0;     // This line of code sets P1.0 as an output pin.
-  P1REN |= BIT3;     // The Resistor Enable bit will be determined by the Button P1.3.
-  P1OUT |= BIT3;     // This line of code sets the resistance to a pullup resistor
+  P1REN |= BIT1;     // The Resistor Enable bit will be determined by the Button P1.3.
+  P1OUT |= BIT1;     // This line of code sets the resistance to a pullup resistor
 
   while(1){
 
       _delay_cycles(200000);        // This line of code will set the toggle delay of the green light to 0.2seconds.
                                     //if the button is hold, it starts to blink.
 
-  if((P1IN & BIT3 ) == 0x00){       // This line of code defines the condition at which the LED changes state.
+  if((P1IN & BIT1 ) == 0x00){       // This line of code defines the condition at which the LED changes state.
   P1OUT ^= BIT0;
 
-  while((P1IN & BIT3) == BIT3);
+  while((P1IN & BIT1) == BIT1);
   }}}
